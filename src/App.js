2cc4c165c93948016c75401e6fe6f97de67aaa54
Sadhna singh <sadhna.singh.work@gmail.com>
2022-12-23 23:47:46 +1300

    added heading

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
-        <h1>Hello AWS</h1>
+        <h1>AWS</h1>
       </header>
     </div>
   )
